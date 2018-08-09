30b251e231346927463f44ea0cceafa5cfc224c4
Viktor Schneider <info@vjs.io>
2018-08-09 13:15:12 +0200

    remove unused folder

--- test/test.cpp
+++ test/test.cpp
@@ -9,18 +9,15 @@ int main() {
         return 1;
     }
 
-    if (!bus) {
-        return 2;
-    }
-
     cctalk::Bus::Command command;
     command.destination = 2;
     command.source = 1;
-    command.header = cctalk::Bus::REQUEST_SOFTWARE_VERSION;
+    command.header = cctalk::Bus::REQUEST_EQUIPMENT_CATEGORY_ID;
 
     bus.send(command);
 
-    bus.receive(1, [] (std::optional<cctalk::Bus::DataCommand> dataCommand) {
+    bus.receive(2, [] (std::optional<cctalk::Bus::DataCommand> dataCommand) {
+        std::cout << "device type: ";
         std::cout.write(reinterpret_cast<char*>(dataCommand->data), dataCommand->length);
         std::cout << std::endl;
     });
